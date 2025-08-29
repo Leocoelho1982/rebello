516e0c54dc7669606a70c4c988e996145f90057d
Rui Diniz <rui.maiodiniz@hotmail.com>
2025-08-29 15:50:21 +0100

    Why faq Contact Footer

---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Contact(){
+    return(
+        <div className="font-bold text-3xl m-5 text-center">Contacto</div>
+    )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <div className="font-bold text-3xl m-5 text-center">Footer</div>
+    )
+}
